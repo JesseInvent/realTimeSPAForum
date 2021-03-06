beafd94fad2c776f22ab697cd7fcadeb644840be
Jesse Ojih <jcinvent05@gmail.com>
2021-03-06 05:37:52 +0100

    CRUD for Reply

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\CategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\QuestionController;
+use App\Http\Controllers\ReplyController;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,3 +24,5 @@ use App\Http\Controllers\QuestionController;
 Route::apiResource('question', QuestionController::class);
 
 Route::apiResource('category', CategoryController::class);
+
+Route::apiResource('question/{question}/reply', ReplyController::class);
